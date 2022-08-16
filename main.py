91d41338cb55178d4f0c6b2bb71449b73e70be80
Almaz2312 <Almazdurusaliev@gmail.com>
2022-08-16 17:19:52 +0600

    optimized my code base on Altynai's method

--- main.py
+++ main.py
@@ -6,56 +6,94 @@ from delete import delete_file, delete_directory
 from list import dirlist
 
 
+base = '/home/almaz/PycharmProjects/zeon/zeon_fs/'
+
+commands = {
+    'touch': touch,
+    'mkdir': mkdir,
+    'rm': delete_directory,
+    'delete': delete_file,
+    'list': dirlist
+}
+
 if __name__ == '__main__':
+    # shows command options
     print("""
             list - to view. 
                 Can use ls for directory to see in current working directory,
-            create - to create directory or a file,
-            delete - to delete directory or a file
+            touch - to create a file,
+            mkdir - to create a directory,
+            delete - to delete a file,
+            rm - to delete a directory
             """)
+
     while True:
-        command = input('> ').lower()
+        args = input('>>> ').split()
+        try:
+            command = commands[args[0]]
+            if args[1:] is isinstance:
+                print(command(base+args[1]))
+            print(command(base))
 
-        if command == 'list':
-            try:
-                subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('directory >> ')
-                if subcommand == '/home/almaz/PycharmProjects/zeon/zeon_fs/ls':
-                    subcommand = 'current'
-                print(dirlist(subcommand))
-            except:
-                print(traceback.print_exc())
+        except:
+            print(traceback.print_exc())
 
-        elif command == 'create':
-            choice = input('Create a file or a directory: ').lower()
-            if choice == 'directory':
-                try:
-                    subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('in directory >> ')
-                    print(mkdir(subcommand))
-                except:
-                    print(traceback.print_exc())
 
-            elif choice == 'file':
-                try:
-                    subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('in directory >> ')
-                    print(touch(subcommand))
-                except:
-                    print(traceback.print_exc())
-        elif command == 'delete':
-            choice = input('Delete a file or a directory: ').lower()
-            if choice == 'directory':
-                try:
-                    subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('path to file or directory >> ')
-                    print(delete_directory(subcommand))
-                except:
-                    print(traceback.print_exc())
-            elif choice == 'file':
-                try:
-                    subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('path to file or directory >> ')
-                    print(delete_file(subcommand))
-                except:
-                    print(traceback.print_exc())
-        elif command == 'quit':
-            break
 
-        else:
-            print('There is no such command')
+
+
+
+
+
+"""
+My first attempt in doing filesystem
+"""
+    ## Work with while loop
+    # while True:
+    #     command = input('> ').lower()
+    #
+    #     # list option
+    #     if command == 'list':
+    #         try:
+    #             subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('directory >> ')
+    #             if subcommand == '/home/almaz/PycharmProjects/zeon/zeon_fs/ls':
+    #                 subcommand = 'current'
+    #             print(dirlist(subcommand))
+    #         except:
+    #             print(traceback.print_exc())
+    #
+    #     # create option
+    #     elif command == 'create':
+    #         choice = input('Create a file or a directory: ').lower()
+    #         if choice == 'directory':
+    #             try:
+    #                 subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('in directory >> ')
+    #                 print(mkdir(subcommand))
+    #             except:
+    #                 print(traceback.print_exc())
+    #
+    #         elif choice == 'file':
+    #             try:
+    #                 subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('in directory >> ')
+    #                 print(touch(subcommand))
+    #             except:
+    #                 print(traceback.print_exc())
+    #     elif command == 'delete':
+    #         choice = input('Delete a file or a directory: ').lower()
+    #         if choice == 'directory':
+    #             try:
+    #                 subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('path to file or directory >> ')
+    #                 print(delete_directory(subcommand))
+    #             except:
+    #                 print(traceback.print_exc())
+    #         elif choice == 'file':
+    #             try:
+    #                 subcommand = '/home/almaz/PycharmProjects/zeon/zeon_fs/' + input('path to file or directory >> ')
+    #                 print(delete_file(subcommand))
+    #             except:
+    #                 print(traceback.print_exc())
+    #     elif command == 'quit':
+    #         break
+    #
+    #     else:
+    #         print('There is no such command')
